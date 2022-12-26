8f4f73f8fa638b8559b1f9ac2a0f3109edf5763a
Kim LeeJun <hwanam1111@naver.com>
2022-12-26 22:00:29 +0900

    remove: 사용하지 않는 dotenv 삭제

--- next.config.js
+++ next.config.js
@@ -5,16 +5,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = withBundleAnalyzer({
   env: {
     FRONTEND_SERVER_URL: process.env.FRONTEND_SERVER_URL,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     BACKEND_SERVER_URL: process.env.BACKEND_SERVER_URL,
-    JWT_SECRET: process.env.JWT_SECRET,
     JWT_COOKIE_NAME: process.env.JWT_COOKIE_NAME,
-    GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
-    GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-    KAKAO_REST_API_KEY: process.env.KAKAO_REST_API_KEY,
-    KAKAO_CLIENT_SECRENT: process.env.KAKAO_CLIENT_SECRENT,
-    NAVER_CLIENT_ID: process.env.NAVER_CLIENT_ID,
-    NAVER_CLIENT_SECRENT: process.env.NAVER_CLIENT_SECRENT,
   },
   webpack(config, { webpack }) {
     const prod = process.env.NODE_ENV === 'prod';
