91a64a63ecd5a23255218037214bf405a357c8a1
Kim LeeJun <hwanam1111@naver.com>
2022-12-28 21:28:22 +0900

    feat: 유저 권한 셋업

--- src/pages/countries/[country]/create-product.tsx
+++ src/pages/countries/[country]/create-product.tsx
@@ -1,12 +1,18 @@
 import RootLayout from '@ui/root-layout';
 import useRouteWhenInvalidCountry from '@hooks/useRouteWhenInvalidCountry';
+import useI18n from '@hooks/useI18n';
+import { I18N_CREATE_PRODUCT } from '@constants/i18n-namespace';
 
 export default function CreateProductPage() {
   useRouteWhenInvalidCountry();
 
+  const { i18n } = useI18n(I18N_CREATE_PRODUCT);
+
   return (
-    <RootLayout metaTitle="상품등록">
-      <h1>상품등록</h1>
+    <RootLayout metaTitle={i18n('meta-title')} accessRole="Manager">
+      <div>
+        <h1>상품 등록</h1>
+      </div>
     </RootLayout>
   );
 }
